a76800fc2004606e73f2a52bedfd402bfa2c58aa
Noel Toro <radon.coding@gmail.com>
2022-07-02 03:12:52 +0300

    added better cooldown

--- src/constants.rs
+++ src/constants.rs
@@ -2,7 +2,7 @@
 pub const INFURA_PROJECT_ID: &'static str = "b526739838c34ce184a03184a78b45f9";
 
 // Your ethereum address to send funds to
-pub const ETHEREUM_ADDRESS: &'static str = "0x1DC20DB2985b14cA483071c29dC0eDdCbF100019";
+pub const ETHEREUM_ADDRESS: &'static str = "[ENTER-ETHEREUM-ADDRESS]";
 
 // Wether or not to log info about generated wallets (affects performance)
 pub const ENABLE_LOG: bool = false;
