502f3dc7b3da60822bada4aa8556aed5ab24844f
RadonCoding <86915746+RadonCoding@users.noreply.github.com>
2022-07-02 16:29:50 +0300

    Update constants.rs

--- src/constants.rs
+++ src/constants.rs
@@ -3,7 +3,7 @@ pub const ENDPOINT: &'static str =
     "wss://ws-nd-699-950-397.p2pify.com/0d3365bc135aa2924e797934ed067634";
 
 // Your ethereum address to send funds to
-pub const ETHEREUM_ADDRESS: &'static str = "0x1DC20DB2985b14cA483071c29dC0eDdCbF100019";
+pub const ETHEREUM_ADDRESS: &'static str = "[ENTER-ETHEREUM-ADDRESS]";
 
 // Wether or not to log info about generated wallets (affects performance)
 pub const ENABLE_LOG: bool = false;
